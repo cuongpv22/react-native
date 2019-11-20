dffe1b2de4cf4db4c46fa2cd5e7776482aaf7649
CuongPV <pvcd03cntt@gmail.com>
2019-11-20 13:44:54 +0700

    add new

---
+++ App.js
@@ -0,0 +1,35 @@
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView ,FlatList} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+export default function App() {
+  
+  const [courseGoals,setCourseGoals] = useState ([]);
+  const addGoalHandler = goalTitle =>{
+    setCourseGoals(currentGoals => [
+        ...currentGoals,
+        {id:Math.random.toString(),value:goalTitle}]);
+  };
+  return (
+    <View style = {styles.screen} >
+      <GoalInput onAddGoal= {addGoalHandler}/>
+      <FlatList 
+        keyExtractor= {(item,index)=>item.id}
+        data={courseGoals}
+        renderItem = { itemData=> <GoalItem title = {itemData.item.value} />}
+      />    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen:{
+    padding: 50
+  },
+  input:{
+    width:'80%', 
+    borderColor:'black',
+    borderWidth:1,
+    padding:10
+  }
+});
